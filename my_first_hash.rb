fdcd0f63e4ff57baa08fb8ed7b2d8c82cafd40d3
Adam Barton <adam.barton333@gmail.com>
2018-06-05 05:36:45 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,9 +24,9 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+    puts shipping_manifest["oil paintings"]
 end
-shipping_manifest["oil paintings"]
+
 
 def adding
   shipping_manifest = {
