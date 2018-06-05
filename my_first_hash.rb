fdc998c57b165d07893e11a264f2da523682a07b
Adam Barton <adam.barton333@gmail.com>
2018-06-05 05:27:19 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,10 +7,10 @@ end
 
 def shipping_manifest
   the_manifest = {}
+end
   the_manifest["whale bone corsets"] => 5
   the_manifest["porcelain vases"] => 2 
   the_manifest["oil paintings"] => 3 
-end
 puts shipping_manifest
 
 def retrieval
